ee3a7a325990d018058cef25e95e8045e5999196
Chris Wang <Chrismiaow@gmail.com>
2018-11-20 10:52:00 +0800

    Update selectionSort

--- AlgorithmConsole/AlgorithmConsole/SelectionSort.cs
+++ AlgorithmConsole/AlgorithmConsole/SelectionSort.cs
@@ -18,26 +18,26 @@ namespace AlgorithmConsole
         {
             var watch = new Stopwatch();
             watch.Start();
+
             for (int i = 0; i < intList.Length; i++)
             {
-                var minVal = intList[i]; // 假设 i 下表就是最小的数
-                var minIndex = i; //记录我认为最小数的下标
-
-                for (int j = i + 1; j < intList.Length; j++) // 这里只是找出这一趟最小的数值 ,并记录下它的下标
+                var min = intList[i];
+                int minIndex = i;
+                for (int j = i+1; j < intList.Length; j++)
                 {
-                    //说明我们认为的最小值, 不是最小
-                    if (minVal > intList[j]) // 这里大于号是升序(大于是找出最小值) 小于是降序(小于是找出最大值)
+                    if (min > intList[j])
                     {
-                        minVal = intList[j]; //更新这趟最小值(最大值)的值,
-                        minIndex = j; // 记录下标
+                        min = intList[j];
+                        minIndex = j;
                     }
                 }
-
-                // 最后把最小的数和第一个位置交换
-                var temp = intList[i]; //把第一个原先认为是最小值的数, 临时保存起来
-                intList[i] = intList[minIndex]; //把最终我们找到的最小值赋给这一趟的比较的第一个位置
-                intList[minIndex] = temp; //把原先保存好临时数值放回这个数组的空地方, 保证数组的完整性.
+                if (minIndex != i)
+                {
+                    intList[minIndex] = intList[i];
+                    intList[i] = min;
+                }
             }
+
             watch.Stop();
             var displayString = string.Empty;
             foreach (var item in intList)
